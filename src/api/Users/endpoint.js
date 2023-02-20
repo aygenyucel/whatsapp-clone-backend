28b944a63a8022b2e39fdd0b640dbcc3b661d6ca
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-02-20 18:48:03 +0300

    adds jwtauth, login&register endpoints

--- src/api/Users/endpoint.js
+++ src/api/Users/endpoint.js
@@ -2,111 +2,138 @@ import express from "express";
 import User from "./model.js";
 import createHttpError from "http-errors";
 import multer from "multer";
+import { createAccessToken } from "../lib/jwt-tools.js";
 
-export const usersRouter = express.Router();
+const usersRouter = express.Router();
 
 const upload = multer({ dest: "uploads/" });
 
 usersRouter.post("/", async (req, res, next) => {
-    try {
-        const newUser = new User(req.body);
-        const { _id } = await newUser.save();
-        res.status(201).send({ _id });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const newUser = new User(req.body);
+    const { _id } = await newUser.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
 });
 //get all users
 usersRouter.get("/", async (req, res, next) => {
-    try {
-        const users = await User.find();
-        res.send(users);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
 });
 //get user by id
 usersRouter.get("/:id", async (req, res, next) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (user) {
-            res.send(user);
-        } else {
-            next(
-                createHttpError(404, `User with id ${req.params.id} not found!`)
-            );
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      next(createHttpError(404, `User with id ${req.params.id} not found!`));
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 //update user by id
 usersRouter.put("/:id", async (req, res, next) => {
-    //
-    try {
-        const modifiedUser = await User.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {
-                runValidators: true,
-                new: true,
-            }
-        );
-        if (modifiedUser) {
-            res.send(modifiedUser);
-        } else {
-            next(
-                createHttpError(404, `User with id ${req.params.id} not found!`)
-            );
-        }
-    } catch (error) {
-        next(error);
+  //
+  try {
+    const modifiedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    if (modifiedUser) {
+      res.send(modifiedUser);
+    } else {
+      next(createHttpError(404, `User with id ${req.params.id} not found!`));
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 //delete user by id
 usersRouter.delete("/:id", async (req, res, next) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (user) {
-            res.status(204).send();
-        } else {
-            next(
-                createHttpError(404, `User with id ${req.params.id} not found!`)
-            );
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (user) {
+      res.status(204).send();
+    } else {
+      next(createHttpError(404, `User with id ${req.params.id} not found!`));
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//user login
+usersRouter.post("/session", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.checkCredentials(email, password);
+    console.log("user:", user);
+
+    if (user) {
+      const payload = { _id: user._id };
+      const accessToken = await createAccessToken(payload);
+      res.send({ accessToken });
+    } else {
+      next(createHttpError(404, "Credentials are not ok!"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//user registiration
+usersRouter.post("/account", async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await User.checkEmail(email);
+    if (user) {
+      next(createHttpError(409, `The user with this email already exist!`));
+    } else {
+      const newUser = new User(req.body);
+      const { _id } = await newUser.save();
+      const payload = { _id };
+      const accessToken = await createAccessToken(payload);
+      res.status(201).send({ accessToken });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 //upload avatar
 usersRouter.post(
-    "/:id/uploadAvatar",
-    upload.single("avatar"),
-    async (req, res, next) => {
-        try {
-            const modifiedUser = await User.findByIdAndUpdate(
-                req.params.id,
-                { avatar: req.file.path },
-                {
-                    runValidators: true,
-                    new: true,
-                }
-            );
-            if (modifiedUser) {
-                res.send(modifiedUser);
-            } else {
-                next(
-                    createHttpError(
-                        404,
-                        `User with id ${req.params.id} not found!`
-                    )
-                );
-            }
-        } catch (error) {
-            next(error);
+  "/:id/uploadAvatar",
+  upload.single("avatar"),
+  async (req, res, next) => {
+    try {
+      const modifiedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        { avatar: req.file.path },
+        {
+          runValidators: true,
+          new: true,
         }
+      );
+      if (modifiedUser) {
+        res.send(modifiedUser);
+      } else {
+        next(createHttpError(404, `User with id ${req.params.id} not found!`));
+      }
+    } catch (error) {
+      next(error);
     }
+  }
 );
+
+export default usersRouter;
--- src/api/Users/model.js
+++ src/api/Users/model.js
@@ -1,17 +1,53 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    avatar: {
-        type: String,
-        required: false,
-        default: "https://via.placeholder.com/200x200",
-    },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  avatar: {
+    type: String,
+    required: false,
+    default: "https://via.placeholder.com/200x200",
+  },
+  password: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+userSchema.pre("save", async function (next) {
+  const currentUser = this;
+  if (currentUser.isModified("password")) {
+    const plainPW = currentUser.password;
+
+    const hash = await bcrypt.hash(plainPW, 10);
+    currentUser.password = hash;
+  }
+  next();
+});
+
+userSchema.static("checkCredentials", async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      return user;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+});
+
+userSchema.static("checkEmail", async function (email) {
+  const user = await this.findOne({ email });
+  if (user) {
+    return email;
+  } else {
+    return null;
+  }
 });
 
 export default model("User", userSchema);
