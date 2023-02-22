fe4f0732bbd214c0f20d01b46403902a6debabed
Morgan <morgan@morganevans.dev>
2023-02-22 10:01:13 +0000

    fixed socket issue

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,12 +1,16 @@
-import { Socket } from "socket.io";
-
 let onlineUsers = [];
 
-export default (socket) => {
+export const socketHandler = (socket) => {
     const clientId = socket.id;
     console.log("Client connected: " + clientId);
 
     socket.on("disconnect", () => {
         console.log("Client disconnected: " + clientId);
+    });
+
+    //on error
+    socket.on("error", (err) => {
+        console.log("received error from client:", clientId);
+        console.log(err);
     });
 };
