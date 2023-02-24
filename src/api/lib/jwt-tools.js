ce6c919004d140ed94434281f8b79b6a6ea32a12
Morgan <morgan@morganevans.dev>
2023-02-24 09:55:23 +0000

    socketio auth chat logic

--- src/api/lib/jwt-tools.js
+++ src/api/lib/jwt-tools.js
@@ -3,74 +3,78 @@ import User from "../Users/model.js";
 import createHttpError from "http-errors";
 
 export const createAccessToken = (payload) =>
-  new Promise((resolve, reject) => {
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: "15m" },
-      (err, token) => {
-        if (err) reject(err);
-        else resolve(token);
-      }
-    );
-  });
+    new Promise((resolve, reject) => {
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: "15m" },
+            (err, token) => {
+                if (err) reject(err);
+                else resolve(token);
+            }
+        );
+    });
 
 export const verifyAccessToken = (token) =>
-  new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.JWT_SECRET, (err, originalPayload) => {
-      if (err) reject(err);
-      else resolve(originalPayload);
+    new Promise((resolve, reject) => {
+        jwt.verify(token, process.env.JWT_SECRET, (err, originalPayload) => {
+            if (err) reject(err);
+            else resolve(originalPayload);
+        });
     });
-  });
 
 export const createRefreshToken = (payload) =>
-  new Promise((resolve, reject) => {
-    jwt.sign(
-      payload,
-      process.env.REFRESH_SECRET,
-      { expiresIn: "1 week" },
-      (err, token) => {
-        if (err) reject(err);
-        else resolve(token);
-      }
-    );
-  });
+    new Promise((resolve, reject) => {
+        jwt.sign(
+            payload,
+            process.env.REFRESH_SECRET,
+            { expiresIn: "1 week" },
+            (err, token) => {
+                if (err) reject(err);
+                else resolve(token);
+            }
+        );
+    });
 
 export const verifyRefreshToken = (token) =>
-  new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.REFRESH_SECRET, (err, originalPayload) => {
-      if (err) reject(err);
-      else resolve(originalPayload);
+    new Promise((resolve, reject) => {
+        jwt.verify(
+            token,
+            process.env.REFRESH_SECRET,
+            (err, originalPayload) => {
+                if (err) reject(err);
+                else resolve(originalPayload);
+            }
+        );
     });
-  });
 
 /*******************/
 
 //to create both tokens and save refresh token in db. Using after user login
 export const createTokens = async (user) => {
-  //creates two tokens (access & refresh)
-  const accessToken = await createAccessToken({ _id: user._id });
-  const refreshToken = await createRefreshToken({ _id: user._id });
-  //refresh token saved in db
-  user.refreshToken = refreshToken;
-  await user.save();
-  return { accessToken, refreshToken };
+    //creates two tokens (access & refresh)
+    const accessToken = await createAccessToken({ _id: user._id });
+    const refreshToken = await createRefreshToken({ _id: user._id });
+    //refresh token saved in db
+    user.refreshToken = refreshToken;
+    await user.save();
+    return { accessToken, refreshToken };
 };
 
 //for the /session/refresh endpoint. To create new tokens with verify refresh token first.
 export const verifyRefreshAndCreateNewTokens = async (currentRefreshToken) => {
-  //checking integrity and expiration date of refresh token
-  const { _id } = await verifyRefreshToken(currentRefreshToken);
+    //checking integrity and expiration date of refresh token
+    const { _id } = await verifyRefreshToken(currentRefreshToken);
 
-  const user = await User.findById(_id);
-  if (user) {
-    if (user.refreshToken && user.refreshToken === currentRefreshToken) {
-      const { accessToken, refreshToken } = await createTokens(user);
-      return { accessToken, refreshToken };
+    const user = await User.findById(_id);
+    if (user) {
+        if (user.refreshToken && user.refreshToken === currentRefreshToken) {
+            const { accessToken, refreshToken } = await createTokens(user);
+            return { accessToken, refreshToken, user };
+        } else {
+            throw createHttpError(401, `Refresh token not valid!`);
+        }
     } else {
-      throw createHttpError(401, `Refresh token not valid!`);
+        throw createHttpError(404, `User with id ${_id} not found!`);
     }
-  } else {
-    throw createHttpError(404, `User with id ${_id} not found!`);
-  }
 };
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,6 +1,37 @@
 let onlineUsers = [];
+import {
+    verifyAccessToken,
+    verifyRefreshAndCreateNewTokens,
+} from "../api/lib/jwt-tools.js";
+import Message from "../api/messages/model.js";
 
 //todo use handshake to verify access token / refresh token
+
+const saveMessage = (text, user) =>
+    new Promise((resolve, reject) => {
+        const message = new Message({
+            sender: user._id,
+            content: {
+                text,
+            },
+        });
+        const returnData = {
+            sender: {
+                _id: user._id,
+                name: user.name,
+                avatar: user.avatar,
+            },
+            messageId: message._id,
+            content: {
+                text,
+            },
+            timestamp: new Date().getTime(),
+        };
+        message.save((err, message) => {
+            if (err) reject(err);
+            else resolve(returnData);
+        });
+    });
 
 export const socketHandler = (socket) => {
     const clientId = socket.id;
@@ -20,9 +51,63 @@ export const socketHandler = (socket) => {
 
     //chat handler
     socket.on("sendMessage", (data) => {
+        const { accessToken, refreshToken } = data;
         console.log(
             `received message from ${clientId}: ${data.message.content.text}`
         );
+
+        if (!accessToken || !refreshToken) {
+            socket.emit("error", "You are not authorized to send messages");
+            return;
+        }
+
+        //verify access token
+        verifyAccessToken(accessToken)
+            .then((user) => {
+                //Access token is valid
+
+                saveMessage(data.message.content.text, user).then(
+                    (returnMessage) => {
+                        socket.broadcast.emit("newMessage", {
+                            message: returnMessage,
+                        });
+                    }
+                );
+            })
+            .catch((err) => {
+                //verify refresh token
+                verifyRefreshAndCreateNewTokens(refreshToken)
+                    .then(
+                        ({
+                            accessToken: newAccessToken,
+                            refreshToken: newRefreshToken,
+                            user,
+                        }) => {
+                            //refresh token is valid
+                            console.log("Refresh token is valid");
+                            //todo send new tokens to client
+                            socket.emit("newTokens", {
+                                accessToken: newAccessToken,
+                                refreshToken: newRefreshToken,
+                            });
+
+                            saveMessage(data.message.content.text, user).then(
+                                (returnMessage) => {
+                                    socket.broadcast.emit("newMessage", {
+                                        message: returnMessage,
+                                    });
+                                }
+                            );
+                        }
+                    )
+                    .catch((err) => {
+                        socket.emit(
+                            "error",
+                            "You are not authorized to send messages"
+                        );
+                    });
+            });
+
         socket.broadcast.emit("newMessage", data.message);
     });
 };
